5701c05f3da36f455ecee6f7ef0c76dc07847e30
Michael Yantosca <mike@archivarius.net>
2017-12-03 14:29:50 -0600

    Swap B to block row major for easier accounting and correctness in non-square cases. @TODO: Work out any remaining bugs.

--- src/cannon-su.c
+++ src/cannon-su.c
@@ -123,15 +123,15 @@ int main(int argc, char *argv[]) {
 			     target_device,                                        // dst device
 			     host_device);                                         // src device
 
-      // Shear block B(y,x) to dB(x,(x+y)%c).
-      // B swaps to block column major, handled by P(y,x).
+      // Shear block B(y,x) to dB((y+x)%c,x).
+      // B remains in block row major, handled by P(x,y).
       omp_target_memcpy_rect(dB, B,                                                // dst, src
 			     sizeof(float),                                        // elems
 			     2,                                                    // dims
 			     (const size_t[2]){ v, w },                            // volume
-			     (const size_t[2]){ x * v, (((x + y) % c) + 1) * w },  // dst offs
+			     (const size_t[2]){ (((y + x) % c) + 1) * v, x * w },  // dst offs
 			     (const size_t[2]){ y * v, x * w },                    // src offs
-			     (const size_t[2]){ b * v, (c + 1) * w },              // dst dims
+			     (const size_t[2]){ (c + 1) * v, b * w },              // dst dims
 			     (const size_t[2]){ c * v, b * w },                    // src dims
 			     target_device,                                        // dst device
 			     host_device);                                         // src device
@@ -161,14 +161,14 @@ int main(int argc, char *argv[]) {
     printf("\n");
   }
   printf("dB = \n");
-  for (i = 0; i < q; i++) {
-    for (j = 0; j < n + w; j++) {
-      printf("%f ", dB[i * (n + w) + j]);
+  for (i = 0; i < q + v; i++) {
+    for (j = 0; j < n; j++) {
+      printf("%f ", dB[i * n + j]);
     }
     printf("\n");
   }
 
-  // Phase 2: Cycle through the blocks and multiply the blocks, shifting A left and B up (technically, left) by one block each iteration.
+  // Phase 2: Cycle through the blocks and multiply the blocks, shifting A left and B up by one block each iteration.
   size_t d;
   for (d = 0; d < c; d++) {
     // Multiply all the blocks for the present iteration.
@@ -178,7 +178,7 @@ int main(int argc, char *argv[]) {
 	for (i = 0; i < u; i++) {
 	  for (k = 0; k < v; k++) {
 	    for (j = 0; j < w; j++) {
-	      dC[x * u * n + i * n + y * w + j] += dA[x * u * (q+v) + i * (q+v) + (y+1) * v + k] * dB[y * (n+w) * v + k * (n+w) + (x+1) * w + j]; 
+	      dC[x * u * n + i * n + y * w + j] += dA[x * u * (q+v) + i * (q+v) + (y+1) * v + k] * dB[(x+1) * v * n + k * n + y * w + j]; 
 	    }
 	  }
 	}
@@ -187,7 +187,7 @@ int main(int argc, char *argv[]) {
     // Cycle A and B.
     for (x = 0; x < b; x++) {
       for (y = 1; y <= c+1; y++) {
-	// Shift A(x,y) to the left 1 block.
+	// Shift A(x,y) left 1 block.
 	omp_target_memcpy_rect(dA, dA,                                               // dst, src
 			       sizeof(float),                                        // elem size
 			       2,                                                    // dims
@@ -198,15 +198,15 @@ int main(int argc, char *argv[]) {
 			       (const size_t[2]){ b * u, (c + 1) * v },              // src dims
 			       target_device,                                        // dst device
 			       target_device);                                       // src device
-	// Shift B(x,y) up (to the left) 1 block.
+	// Shift B(x,y) up 1 block.
 	omp_target_memcpy_rect(dB, dB,                                               // dst, src
 			       sizeof(float),                                        // elems
 			       2,                                                    // dims
 			       (const size_t[2]){ v, w },                            // volume
-			       (const size_t[2]){ x * v, ((y - 1) % (c+1)) * w },    // dst offs
-			       (const size_t[2]){ x * v, (y % (c+1)) * w },          // src offs
-			       (const size_t[2]){ b * v, (c + 1) * w },              // dst dims
-			       (const size_t[2]){ b * v, (c + 1) * w },              // src dims
+			       (const size_t[2]){ ((y - 1) % (c+1)) * v, x * w },    // dst offs
+			       (const size_t[2]){ (y % (c+1)) * v, x * w },          // src offs
+			       (const size_t[2]){ (b + 1) * v, c * w },              // dst dims
+			       (const size_t[2]){ (b + 1) * v, c * w },              // src dims
 			       target_device,                                        // dst device
 			       target_device);                                       // src device
       }
@@ -219,9 +219,9 @@ int main(int argc, char *argv[]) {
       printf("\n");
     }
     printf("[%lu] dB = \n", d);
-    for (i = 0; i < q; i++) {
-      for (j = 0; j < n + w; j++) {
-	printf("%f ", dB[i * (n + w) + j]);
+    for (i = 0; i < q + v; i++) {
+      for (j = 0; j < n; j++) {
+	printf("%f ", dB[i * n + j]);
       }
       printf("\n");
     }
