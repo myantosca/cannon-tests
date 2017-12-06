3e72ffd28322021778490c19718328a4a024d933
Michael Yantosca <mike@archivarius.net>
2017-12-05 23:30:32 -0600

    Store output of mkl_cblas_sgemm test runs.

--- run_mkl_cblas_sgemm.sh
+++ run_mkl_cblas_sgemm.sh
@@ -4,6 +4,11 @@
 #SBATCH -N 1
 #SBATCH --ntasks-per-node 1
 
+export TS="$(date +%yy-%m-%d.%H.%M.%S)"
+export DOUT="./results/matrixMulCUBLAS/k80/${TS}"
+
+mkdir -p ${DOUT}
+
 for ((d = 256; d < 65536; d *= 4)); do
-    ./mkl_cblas_sgemm -m $d -q $d -n $d -s 30
+    ./mkl_cblas_sgemm -m $d -q $d -n $d -s 30 1> ./mkl_cblas_sgemm.$d.csv 2> /dev/null
 done
