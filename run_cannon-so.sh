6d0f09b59e89fd54e41e4cf4686914a59a247ba7
Michael Yantosca <mike@archivarius.net>
2017-12-10 21:20:20 -0600

    Change order of MPI process count variable in cannon-so script.

--- run_cannon-so.sh
+++ run_cannon-so.sh
@@ -9,7 +9,7 @@ export DOUT="./results/cannon-so/${TS}"
 
 mkdir -p ${DOUT}
 
-for ((p = 1; p <= 256; p *=4)); do
+for ((p = 256; p >=1; p /=4)); do
     for ((d = 256; d <= 16384; d *= 4)); do
 	mpirun -np $p ./cannon-so -m $d -q $d -n $d -s 10 1> ${DOUT}/cannon-so.$p.$d.csv 2> /dev/null
     done
