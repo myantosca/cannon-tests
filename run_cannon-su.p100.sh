3fc3182d90eb0de24cfa08867966f5dcd8c25371
Michael Yantosca <mike@archivarius.net>
2017-12-10 19:48:04 -0600

    Fix run script output and capture stderr if necessary.

--- run_cannon-su.p100.sh
+++ run_cannon-su.p100.sh
@@ -5,13 +5,13 @@
 #SBATCH --ntasks-per-node 1
 #SBATCH --gres=gpu:p100:2
 
-export TS="$(date +%yy-%m-%d.%H.%M.%S)"
+export TS="$(date +%Y-%m-%d.%H.%M.%S)"
 export DOUT="./results/cannon-su/p100/${TS}"
 
 mkdir -p ${DOUT}
 
 for ((p = 1; p <= 256; p *=4)); do
     for ((d = 256; d <= 16384; d *= 4)); do
-	./cannon-su -p $p -m $d -q $d -n $d -s 10 1> ${DOUT}/cannon-su.p100.$d.csv 2> /dev/null
+	./cannon-su -p $p -m $d -q $d -n $d -s 10 1> ${DOUT}/cannon-su.p100.$p.$d.csv 2> ${DOUT}/cannon-su.p100.$p.$d.err
     done
 done
