d19f8a035e0de64b4ab1dfb95aa2fc557997d078
Michael Yantosca <mike@archivarius.net>
2017-12-13 01:47:19 -0600

    Updated copies of sbatch scripts as used on BRIDGES.

--- run_cannon-su.k80.sh
+++ run_cannon-su.k80.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH -p GPU
-#SBATCH -t 04:00:00
+#SBATCH -t 08:00:00
 #SBATCH -N 1
 #SBATCH --ntasks-per-node 1
 #SBATCH --gres=gpu:k80:4
@@ -10,8 +10,8 @@ export DOUT="./results/cannon-su/k80/${TS}"
 
 mkdir -p ${DOUT}
 
-for ((p = 1; p <= 256; p *=4)); do
-    for ((d = 256; d <= 16384; d *= 4)); do
+for ((d = 256; d <= 16384; d *= 4)); do
+    for ((p = 256; p >= 1; p /=4)); do
 	./cannon-su -p $p -m $d -q $d -n $d -s 10 1> ${DOUT}/cannon-su.k80.$p.$d.csv 2> ${DOUT}/cannon-su.k80.$p.$d.err
     done
 done
